61c78f03f9e0c1b97a6ac045d223d09b057ac54a
Walter <wramirez@nsoft.com>
2020-08-20 13:02:37 -0400

    Building Entity Classes

---
+++ ACM.BL/Tests/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9ef994a-7cc4-43cd-ae91-1f040e9a38ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ACM.BL/Tests/ACM.BLTest/customerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            var customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer
+            {
+                LastName = "Baggins"
+            };
+            string expected = "Baggins";
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        //Testing Static Methods
+        public void StaticTest()
+        {
+            var c1= new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount++;
+
+            var c2 = new Customer();
+            c1.FirstName = "Rosie";
+            Customer.InstanceCount++;
+
+            var c3 = new Customer();
+            c1.FirstName = "Frodo";
+            Customer.InstanceCount++;
+           
+
+
+            Assert.AreEqual(3, Customer.InstanceCount);
+
+
+        }
+    }
+}
